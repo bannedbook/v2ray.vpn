af99cc8d5a47994373852c9a9ddda15315f1e5f7
bannedbook <banned.ebook@gmail.com>
2021-09-09 19:25:13 +0800

    update

--- gitupdate.bat
+++ gitupdate.bat
@@ -3,6 +3,6 @@ git pull origin master
 git add -A
 git commit -m "update"
 git push origin master
-git tag -a v6.0.9 -m "release v6.0.9"
+git tag -a v6.1 -m "release v6.1"
 git push origin --tags
 pause
